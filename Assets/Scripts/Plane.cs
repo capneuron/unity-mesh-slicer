abf6775eef7ea588e03f63acb486fc49b5c840e0
yuanfang <ry.fang@outlook.com>
2022-05-10 17:49:18 -0400

    fix some corner cases

--- Assets/Scripts/Plane.cs
+++ Assets/Scripts/Plane.cs
@@ -2,7 +2,105 @@
 
 namespace Slicing
 {
-    public enum IntersectionResult { Zero, One, Two, } 
+    public enum LineXPlaneType
+    {
+        None, One, V1OnPlane, V2OnPlane, TwoOnPlane, 
+    }
+    public struct LineXPlaneResult
+    {
+        public LineXPlaneType type;
+        public Vector3[] intersectionPoints;
+        public float[] k;
+        public LineXPlaneResult(LineXPlaneType type, Vector3[] intersectionPoints, float[] k)
+        {
+            this.type = type;
+            this.intersectionPoints = intersectionPoints;
+            this.k = k;
+        }
+        static public TriXPlaneType TriXPaneIntersection(LineXPlaneResult rab, LineXPlaneResult rac, LineXPlaneResult rbc)
+        {
+            if (rab.type == LineXPlaneType.None &&
+                rac.type == LineXPlaneType.None &&
+                rbc.type == LineXPlaneType.None)
+                return TriXPlaneType.NoIntersection;
+            //-----
+            if (rab.type == LineXPlaneType.None &&
+                rac.type == LineXPlaneType.One &&
+                rbc.type == LineXPlaneType.One)
+                return TriXPlaneType.Cross;
+            
+            if (rab.type == LineXPlaneType.One &&
+                rac.type == LineXPlaneType.None &&
+                rbc.type == LineXPlaneType.One)
+                return TriXPlaneType.Cross;
+            
+            if (rab.type == LineXPlaneType.One &&
+                rac.type == LineXPlaneType.One &&
+                rbc.type == LineXPlaneType.None)
+                return TriXPlaneType.Cross;
+            //----------- uncommon situation ↓↓↓↓ ------------
+            if (rab.type == LineXPlaneType.TwoOnPlane &&
+                rac.type == LineXPlaneType.TwoOnPlane &&
+                rbc.type == LineXPlaneType.TwoOnPlane)
+                return TriXPlaneType.Three;
+            
+            if ((rab.type == LineXPlaneType.TwoOnPlane)||
+                (rac.type == LineXPlaneType.TwoOnPlane) ||
+                rbc.type == LineXPlaneType.TwoOnPlane)
+                return TriXPlaneType.Two;
+            
+            //-----
+            if ((rab.type == LineXPlaneType.V1OnPlane)&&
+                (rac.type == LineXPlaneType.V1OnPlane) &&
+                rbc.type == LineXPlaneType.None)
+                return TriXPlaneType.One;
+            
+            if ((rab.type == LineXPlaneType.V1OnPlane)&&
+                (rac.type == LineXPlaneType.V1OnPlane) &&
+                rbc.type == LineXPlaneType.One)
+                return TriXPlaneType.OneAndCross;
+            // --
+            if ((rab.type == LineXPlaneType.V2OnPlane)&&
+                (rac.type == LineXPlaneType.None) &&
+                rbc.type == LineXPlaneType.V1OnPlane)
+                return TriXPlaneType.One;
+            
+            if ((rab.type == LineXPlaneType.V2OnPlane)&&
+                (rac.type == LineXPlaneType.One) &&
+                rbc.type == LineXPlaneType.V1OnPlane)
+                return TriXPlaneType.OneAndCross;
+            //--
+            if ((rab.type == LineXPlaneType.None)&&
+                (rac.type == LineXPlaneType.V2OnPlane) &&
+                rbc.type == LineXPlaneType.V2OnPlane)
+                return TriXPlaneType.One;
+            
+            if ((rab.type == LineXPlaneType.One)&&
+                (rac.type == LineXPlaneType.V2OnPlane) &&
+                rbc.type == LineXPlaneType.V2OnPlane)
+                return TriXPlaneType.OneAndCross;
+            return TriXPlaneType.NoIntersection;
+        }
+    }
+    
+    public enum TriXPlaneType
+    {
+        Cross, OneAndCross, One, Two, Three, NoIntersection
+    }
+    
+    // public struct TriXPlaneResult
+    // {
+    //     public TriXPlaneType type;
+    //     public Vector3[] intersectionPoints;
+    //     public float[] k;
+    //     public LineXPlaneResult(LineXPlaneType type, Vector3[] intersectionPoints, float[] k)
+    //     {
+    //         this.type = type;
+    //         this.intersectionPoints = intersectionPoints;
+    //         this.k = k;
+    //     }
+    // }
+    
     public class Plane
     {
         public Vector3 normal, point;
@@ -19,38 +117,57 @@ namespace Slicing
         }
         
         //find the intersection point
-        public IntersectionResult Intersection(Vector3 a, Vector3 b, out float k, out Vector3 intersectionPoint)
+        public LineXPlaneResult Intersection(Vector3 a, Vector3 b)
         {
-            intersectionPoint = Vector3.zero;
-            k = 0;
-            var apnXbpn = Vector3.Dot((a - point), normal) * Vector3.Dot((b - point), normal);
-            if (apnXbpn > 0) //TODO: == 0?
+            var apXn = Vector3.Dot((a - point), normal);
+            var bpXn = Vector3.Dot((b - point), normal);
+            var apnXbpn = apXn * bpXn;
+            // two points are on the plane
+            if (Mathf.Abs(apXn) < Mathf.Epsilon && Mathf.Abs(bpXn) < Mathf.Epsilon) //TODO float == 0?
             {
-                return IntersectionResult.Zero;
-            }
-            if (apnXbpn == 0) // two points of the line are on the plane
-            {
-                k = 0.5f;
-                intersectionPoint = a + k*(b - a);
-                return IntersectionResult.Two;
-            }
-            else
-            {
-                k = Vector3.Dot((point - a), normal) / Vector3.Dot((b - a), normal);
-                k = Mathf.Clamp01(k); //TODO ?
+                return new LineXPlaneResult(LineXPlaneType.TwoOnPlane,
+                    new Vector3[] {a, b},
+                    new float[] {0, 0});
             }
             
-            intersectionPoint = a + k*(b - a);
-            return IntersectionResult.One;
+            // one point are on the plane
+            if (Mathf.Abs(apXn) < Mathf.Epsilon) 
+            {
+                return new LineXPlaneResult(LineXPlaneType.V1OnPlane,
+                    new Vector3[] {a},
+                    new float[] {0});
+            }
+            if (Mathf.Abs(bpXn) < Mathf.Epsilon) 
+            {
+                return new LineXPlaneResult(LineXPlaneType.V2OnPlane,
+                    new Vector3[] {b},
+                    new float[] {0});
+            }
+            
+            // no intersection point
+            if (apnXbpn > 0)
+            {
+                return new LineXPlaneResult(LineXPlaneType.None,
+                    null, null);
+            }
+            
+            // one intersection point
+            float k = Vector3.Dot((point - a), normal) / Vector3.Dot((b - a), normal);
+            k = Mathf.Clamp01(k); //TODO ?
+            
+            Vector3 intersectionPoint = a + k*(b - a);
+            return new LineXPlaneResult(LineXPlaneType.One,
+                new Vector3[] {intersectionPoint},
+                new float[] {k});
         }
 
         public int sideOf(Vector3 point)
         {
             var value = Vector3.Dot((point - this.point), this.normal);
+            if (Mathf.Abs(value) < Mathf.Epsilon)
+                return 0;
             if(value > 0)
                 return 1;
-            if (value == 0)
-                return 0;
             return -1;
         }
     }
--- Assets/Scripts/Slicer.cs
+++ Assets/Scripts/Slicer.cs
@@ -1,5 +1,8 @@
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 namespace Slicing
 {
@@ -29,7 +32,7 @@ namespace Slicing
             // iterate triangles
             Mesh mesh = mf.mesh;
             List<MeshVertex> newMeshVertices = MeshVertex.ReadFromMesh(mesh);
-            var newTriangles = new List<int>();
+            List<int> newTriangles = new List<int>();
             
             for (int vi = 0; vi < mesh.triangles.Length; vi += 3)
             {
@@ -42,81 +45,29 @@ namespace Slicing
                 Vector3 c = mesh.vertices[ci];
                 
                 float kab, kac, kbc;
-                var iab = plane.Intersection(a, b, out kab, out var ipab);
-                var iac = plane.Intersection(a, c, out kac, out var ipac);
-                var ibc = plane.Intersection(b, c, out kbc, out var ipbc);
+                var rab = plane.Intersection(a, b);
+                var rac = plane.Intersection(a, c);
+                var rbc = plane.Intersection(b, c);
 
-                //choose a tip and two other vertices as two points the the edge
-                //                ^   a
-                //              /   \
-                //        -------------------
-                //           /        \
-                //       c /___________\ b
-                int tipi = default, e0i = default, e1i = default; //tip, edge point0, edge point1
-                Vector2 uv0 = default, uv1 = default; // uv for the intersection points
-                Vector3 i0 = default, i1 = default; // intersection points
-                Vector3 n0 = default, n1 = default; // normals of intersection points
-                
-                // deciding which point of (a,b,c) is the tip.
-                // calculating uv = a + k(b-a)
-                
-                //copy all old vertices, then add intersection points as new vertices
-                //make a new triangles list
-                
-                if (iab==IntersectionResult.One && iac==IntersectionResult.One)
+                // no intersection
+                TriXPlaneType result = LineXPlaneResult.TriXPaneIntersection(rab, rac, rbc);
+                if(result == TriXPlaneType.NoIntersection || result == TriXPlaneType.One || result == TriXPlaneType.Two || result == TriXPlaneType.Three)
                 {
-                    tipi = ai; 
-                    e0i = bi; i0 = ipab; 
-                    uv0 = mesh.uv[ai] + kab*(mesh.uv[bi]-mesh.uv[ai]);
-                    n0 = (mesh.normals[ai] + mesh.normals[bi]).normalized;
-                    
-                    e1i = ci;  i1 = ipac; 
-                    uv1 = mesh.uv[ai] + kac*(mesh.uv[ci]-mesh.uv[ai]);
-                    n1 = (mesh.normals[ai] + mesh.normals[ci]).normalized;
-                }else if (iab==IntersectionResult.One && ibc==IntersectionResult.One)
-                {
-                    tipi = bi; 
-                    e0i = ci; i0 = ipbc; uv0 = mesh.uv[bi] + kbc*(mesh.uv[ci]-mesh.uv[bi]);
-                    n0 = (mesh.normals[ci] + mesh.normals[bi]).normalized;
-                    
-                    e1i = ai; i1 = ipab; uv1 = mesh.uv[ai] + kab*(mesh.uv[bi]-mesh.uv[ai]);
-                    n1 = (mesh.normals[ai] + mesh.normals[bi]).normalized;
-                }else if (iac==IntersectionResult.One && ibc==IntersectionResult.One)
-                {
-                    tipi = ci;
-                    e0i = ai; i0 = ipac; uv0 = mesh.uv[ai] + kac*(mesh.uv[ci]-mesh.uv[ai]);
-                    n0 = (mesh.normals[ai] + mesh.normals[ci]).normalized;
-                    
-                    e1i = bi; i1 = ipbc; uv1 = mesh.uv[bi] + kbc*(mesh.uv[ci]-mesh.uv[bi]);
-                    n1 = (mesh.normals[ci] + mesh.normals[bi]).normalized;
-                }
-                else if(iab==IntersectionResult.Zero && iac==IntersectionResult.Zero && ibc==IntersectionResult.Zero)
-                {
-                    //no intersection
                     newTriangles.Add(ai);
                     newTriangles.Add(bi);
                     newTriangles.Add(ci);
                     continue;
                 }
 
+                if (result == TriXPlaneType.Cross)
+                {
+                    SliceTo3(mesh, new []{a,b,c}, new []{ai,bi,ci}, new []{rab,rac,rbc}, newMeshVertices, newTriangles);
+                }
+                else if (result == TriXPlaneType.OneAndCross)
+                {
+                    SliceTo2(mesh, new []{a,b,c}, new []{ai,bi,ci}, new []{rab,rac,rbc}, newMeshVertices, newTriangles);
+                }
                 intersect = true;
-                //adding new triangles and vertices
-                var i0i = newMeshVertices.Count;
-                var i1i = i0i+1;
-                newMeshVertices.Add(new MeshVertex(i0, uv0,n0));
-                newMeshVertices.Add(new MeshVertex(i1, uv1,n1));
-                
-                newTriangles.Add(tipi);
-                newTriangles.Add(i0i);
-                newTriangles.Add(i1i);
-                
-                newTriangles.Add(i0i);
-                newTriangles.Add(e0i);
-                newTriangles.Add(i1i);
-                
-                newTriangles.Add(e0i);
-                newTriangles.Add(e1i);
-                newTriangles.Add(i1i);
             }
 
             if (!intersect) return false;
@@ -146,6 +97,7 @@ namespace Slicing
                 }
                 else //side == 0, on the plane
                 {
+                    // Debug.Log("side==0");
                     oldNewIdxDict1[i] = ve1.Count;
                     ve1.Add(newMeshVertices[i]);
                     oldNewIdxDict2[i] = ve2.Count;
@@ -194,6 +146,162 @@ namespace Slicing
             part1.name = obj.name + "1";
             part2.name = obj.name + "2";
             return true;
+        }
+
+        private void SliceTo3(Mesh mesh, Vector3[] abc, int[] abci, LineXPlaneResult[] rabc, List<MeshVertex> newMeshVertices, List<int> newTriangles)
+        {
+            Vector3 a = abc[0];
+            Vector3 b = abc[1]; 
+            Vector3 c = abc[2];
+            
+            int ai = abci[0];
+            int bi = abci[1]; 
+            int ci = abci[2]; 
+            
+            LineXPlaneResult rab = rabc[0];
+            LineXPlaneResult rac = rabc[1]; 
+            LineXPlaneResult rbc = rabc[2];
+
+            //choose a tip and two other vertices as two points the the edge
+            //                /\   a(tip)
+            //              /   \
+            //        -------------------
+            //           /        \
+            // (e0i) c /___________\ b (e1i)
+            int tipi = default, e0i = default, e1i = default; //tip, edge point0, edge point1
+            Vector3 i0 = default, i1 = default; // intersection points
+            Vector2 uv0 = default, uv1 = default; // uv for the intersection points
+            Vector3 n0 = default, n1 = default; // normals of intersection points
+            
+            // deciding which point of (a,b,c) is the tip.
+            // calculating uv = a + k(b-a)
+            
+            //copy all old vertices, then add intersection points as new vertices
+            //make a new triangles list
+            
+            if (rab.type==LineXPlaneType.One && rac.type==LineXPlaneType.One)
+            {
+                tipi = ai; 
+                e0i = bi; i0 = rab.intersectionPoints[0]; 
+                uv0 = mesh.uv[ai] + rab.k[0]*(mesh.uv[bi]-mesh.uv[ai]);
+                n0 = (mesh.normals[ai] + mesh.normals[bi]).normalized;
+                
+                e1i = ci;  i1 = rac.intersectionPoints[0]; 
+                uv1 = mesh.uv[ai] + rac.k[0]*(mesh.uv[ci]-mesh.uv[ai]);
+                n1 = (mesh.normals[ai] + mesh.normals[ci]).normalized;
+            }else if (rab.type==LineXPlaneType.One && rbc.type==LineXPlaneType.One)
+            {
+                tipi = bi; 
+                e0i = ci; i0 = rbc.intersectionPoints[0]; uv0 = mesh.uv[bi] + rbc.k[0]*(mesh.uv[ci]-mesh.uv[bi]);
+                n0 = (mesh.normals[ci] + mesh.normals[bi]).normalized;
+                
+                e1i = ai; i1 = rab.intersectionPoints[0]; uv1 = mesh.uv[ai] + rab.k[0]*(mesh.uv[bi]-mesh.uv[ai]);
+                n1 = (mesh.normals[ai] + mesh.normals[bi]).normalized;
+            }else if (rac.type==LineXPlaneType.One && rbc.type==LineXPlaneType.One)
+            {
+                tipi = ci;
+                e0i = ai; i0 = rac.intersectionPoints[0]; uv0 = mesh.uv[ai] + rac.k[0]*(mesh.uv[ci]-mesh.uv[ai]);
+                n0 = (mesh.normals[ai] + mesh.normals[ci]).normalized;
+                
+                e1i = bi; i1 = rbc.intersectionPoints[0]; uv1 = mesh.uv[bi] + rbc.k[0]*(mesh.uv[ci]-mesh.uv[bi]);
+                n1 = (mesh.normals[ci] + mesh.normals[bi]).normalized;
+            }
+            else
+            {
+                Debug.LogError("Wrong type in SliceTo3()");
+            }
+            
+            //adding new triangles and vertices
+            var i0i = newMeshVertices.Count;
+            var i1i = i0i+1;
+            newMeshVertices.Add(new MeshVertex(i0, uv0,n0));
+            newMeshVertices.Add(new MeshVertex(i1, uv1,n1));
+            
+            newTriangles.Add(tipi);
+            newTriangles.Add(i0i);
+            newTriangles.Add(i1i);
+            
+            newTriangles.Add(i0i);
+            newTriangles.Add(e0i);
+            newTriangles.Add(i1i);
+            
+            newTriangles.Add(e0i);
+            newTriangles.Add(e1i);
+            newTriangles.Add(i1i);
+        }
+        
+         private void SliceTo2(Mesh mesh, Vector3[] abc, int[] abci, LineXPlaneResult[] rabc, List<MeshVertex> newMeshVertices, List<int> newTriangles)
+        {
+            Vector3 a = abc[0];
+            Vector3 b = abc[1]; 
+            Vector3 c = abc[2];
+            
+            int ai = abci[0];
+            int bi = abci[1]; 
+            int ci = abci[2]; 
+            
+            LineXPlaneResult rab = rabc[0];
+            LineXPlaneResult rac = rabc[1]; 
+            LineXPlaneResult rbc = rabc[2];
+
+            //choose a tip and two other vertices as two points the the edge
+            //                  /\
+            //                / | \   a(tip)
+            //              /  |   \
+            //           /    |     \
+            // (e0i) c /_____|_______\ b (e1i)
+            int tipi = default, e0i = default, e1i = default; //tip, edge point0, edge point1
+            Vector3 inter = default; // intersection points
+            Vector2 uv = default; // uv for the intersection points
+            Vector3 normal = default; // normals of intersection points
+            
+            // deciding which point of (a,b,c) is the tip.
+            // calculating uv = a + k(b-a)
+            
+            //copy all old vertices, then add intersection points as new vertices
+            //make a new triangles list
+            
+            if (rbc.type==LineXPlaneType.One) // a is the tip
+            {
+                tipi = ai; 
+                inter = rbc.intersectionPoints[0]; uv = mesh.uv[bi] + rbc.k[0]*(mesh.uv[ci]-mesh.uv[bi]);
+                normal = (mesh.normals[ci] + mesh.normals[bi]).normalized;
+                
+                e0i = bi; 
+                e1i = ci; 
+            }else if (rac.type==LineXPlaneType.One)
+            {
+                tipi = bi; 
+                inter = rac.intersectionPoints[0]; uv = mesh.uv[ai] + rac.k[0]*(mesh.uv[ci]-mesh.uv[ai]);
+                normal = (mesh.normals[ai] + mesh.normals[ci]).normalized;
+                
+                e0i = ci;
+                e1i = ai;
+            }else if (rab.type==LineXPlaneType.One)
+            {
+                tipi = ci;
+                inter = rab.intersectionPoints[0]; uv = mesh.uv[ai] + rab.k[0]*(mesh.uv[bi]-mesh.uv[ai]);
+                normal = (mesh.normals[ai] + mesh.normals[bi]).normalized;
+                
+                e0i = ai;
+                e1i = bi;
+            }
+            else
+            {
+                Debug.LogError("Wrong type in SliceTo3()");
+            }
+            
+            //adding new triangles and vertices
+            var interIdx = newMeshVertices.Count;
+            newMeshVertices.Add(new MeshVertex(inter, uv,normal));
+            
+            newTriangles.Add(tipi);
+            newTriangles.Add(e0i);
+            newTriangles.Add(interIdx);
+            
+            newTriangles.Add(interIdx);
+            newTriangles.Add(e1i);
+            newTriangles.Add(tipi);
         }
         
         public GameObject CreateMeshFromOrigin(List<MeshVertex> vertices, int[] triangles, GameObject origin)
